1b894fe29ac35c95dd46897b101743c8da446a1b
Dmitrii Lobanov <oftkor@gmail.com>
2022-08-06 00:14:11 +0300

    Initial Commit

---
+++ iOS_diplom_VK_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_diplom_VK_App
+//
+//  Created by Dmitrii Lobanov on 06.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
