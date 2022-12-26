99c353118cb68b01d192738e54326e2e9fb29ab1
Dmitriy Lobanov <dvmald@icloud.com>
2022-12-26 18:37:11 +0700

    code review

---
+++ iOS_diplom_VK_App/NewsFeed/FavouriteNewsViewController.swift
@@ -0,0 +1,88 @@
+//
+//  FavouriteNewsViewController.swift
+//  iOS_diplom_VK_App
+//
+//  Created by Dmitrii Lobanov on 20.12.2022.
+//
+
+import UIKit
+
+class FavouriteNewsViewController: UIViewController {
+    
+    private var feedViewModel = FeedViewModel(posts: [])
+    
+    // MARK: UI
+    lazy var feedTableView: UITableView = {
+        let table = UITableView()
+        return table
+    }()
+    
+    // MARK: Setup
+    private func UISetup() {
+        view.addSubviews(views: feedTableView)
+        feedTableView.separatorStyle = .none
+        feedTableView.backgroundColor = .clear
+        feedTableView.delegate = self
+        feedTableView.dataSource = self
+        feedTableView.register(NewsFeedCell.self, forCellReuseIdentifier: NewsFeedCell.id)
+        feedTableView.estimatedRowHeight =  UITableView.automaticDimension
+        feedTableView.topToSuperview(usingSafeArea: true)
+        feedTableView.bottomToSuperview(offset: FeedVCConstants.tableViewBottomOffset, usingSafeArea: true)
+        feedTableView.widthToSuperview()
+    }
+
+
+    // MARK: View lifecycle
+    override func viewWillAppear(_ animated: Bool) {
+        reloadData()
+    }
+    
+    @objc func reloadFavourite() {
+        reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        UISetup()
+        // Наблюдатели
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadFavourite), name: NSNotification.Name(rawValue: "reloadFavourite"), object: nil)
+       
+    }
+    
+    private func reloadData() {
+        LocalStorage.shared.getFavouritePost()
+        self.feedViewModel = LocalStorage.shared.favouriteViewModel ?? FeedViewModel(posts: [])
+        feedTableView.reloadData()
+    }
+}
+
+extension FavouriteNewsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return feedViewModel.posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NewsFeedCell.id, for: indexPath) as! NewsFeedCell
+        guard var feedViewModel = LocalStorage.shared.favouriteViewModel?.posts[indexPath.row] else { return cell }
+       
+        cell.setupCell(viewModel: feedViewModel)
+        cell.layoutSubviews()
+        cell.tapLike = {
+            LocalStorage.shared.likeStatusUpdate(index: indexPath.row, typePage: .Favourite)
+            feedViewModel = (LocalStorage.shared.favouriteViewModel?.posts[indexPath.row])!
+            cell.setupCell(viewModel: feedViewModel)
+            NotificationCenter.default.post(name: Notification.Name("reloadFavourite"), object: nil)
+            NotificationCenter.default.post(name: Notification.Name("reloadNews"), object: nil)
+            cell.layoutSubviews()
+            }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let cellViewModel = feedViewModel.posts[indexPath.row]
+        return cellViewModel.totalHeight ?? 0
+    }
+}
+
